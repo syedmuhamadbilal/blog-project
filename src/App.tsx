d8bb5933c7ff631a230f83d0890b39e90476f48e
thewasif <wasif33@outlook.com>
2025-08-11 11:28:50 +0500

    Redux setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,47 @@
+import { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/Slices/AuthSlice/AuthSlice";
+import authService from "./services/auth/auth";
+import { Header, Footer } from "./components/index.";
+import "./components/CSS/Global.css";
+import { Outlet } from "react-router-dom";
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
   console.log(import.meta.env.VITE_APPWRITE_URL, "CHECK ENV");
-
-  return <>Name</>;
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((user) => {
+        if (user) {
+          dispatch(login({ userData: user }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => {
+        setLoading(false);
+        console.log("User checked");
+      });
+  }, [dispatch]);
+  return (
+    <>
+      {loading ? (
+        <div className="min-h-screen flex flex-wrap justify-between bg-gray-400">
+          <div className="w-full block">
+            <Header />
+            <main>{/* <Outlet /> */}</main>
+            <Footer />
+          </div>
+        </div>
+      ) : (
+        <h1>Chalta ha bhai</h1>
+      )}
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/configuration/configuration.ts
@@ -0,0 +1,139 @@
+import config from "../../config/config";
+import { Client, Databases, Storage, Query, ID } from "appwrite";
+
+export class ConfigurationService {
+  client = new Client();
+  databases;
+  bucket;
+  constructor() {
+    this.client
+      .setEndpoint(config.appWriteUrl)
+      .setProject(config.appWriteProjectId);
+    this.databases = new Databases(this.client);
+    this.bucket = new Storage(this.client);
+  }
+  async createPost(
+    title: string,
+    slug: string,
+    content: string,
+    featuredImage: string,
+    userId: string,
+    status: string
+  ) {
+    try {
+      return await this.databases.createDocument(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async updatePost(
+    title: string,
+    slug: string,
+    content: string,
+    featuredImage: string,
+    userId: string,
+    status: string
+  ) {
+    try {
+      return await this.databases.updateDocument(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        slug,
+        {
+          title,
+          content,
+          featuredImage,
+          status,
+          userId,
+        }
+      );
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async deletePost(slug: string) {
+    try {
+      await this.databases.deleteDocument(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to delete post");
+      return false;
+    }
+  }
+  async getPost(slug: string) {
+    try {
+      await this.databases.getDocument(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        slug
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to get post");
+      return false;
+    }
+  }
+  async getAllPost(queries = [Query.equal("status", "active")]) {
+    try {
+      await this.databases.listDocuments(
+        config.appWriteDatabaseId,
+        config.appWriteCollectionId,
+        queries
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to get post");
+      return false;
+    }
+  }
+  // FILE UPLOAD SERVICES
+
+  async uploadFile(file: File) {
+    try {
+      return await this.bucket.createFile(
+        config.appWriteBucketId,
+        ID.unique(),
+        file
+      );
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to upload File");
+      return false;
+    }
+  }
+  async deleteFile(fileId: string) {
+    try {
+      return await this.bucket.deleteFile(config.appWriteBucketId, fileId);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to delete file");
+    }
+  }
+  async getFilePreview(fieldId: string) {
+    try {
+      await this.bucket.getFile(config.appWriteBucketId, fieldId);
+    } catch (error) {
+      console.log(error);
+      throw new Error("Failed to get file");
+    }
+  }
+}
+
+const configurationService = new ConfigurationService();
+
+export default configurationService;
